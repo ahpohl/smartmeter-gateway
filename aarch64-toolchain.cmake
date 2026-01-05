d36b29355a766ee5db79ed5e8f7492f2d9740837
Alexander Pohl <alex@ahpohl.com>
2026-01-05 13:51:34 +0100

    remove extra sysroot include in toolchain file

--- aarch64-toolchain.cmake
+++ aarch64-toolchain.cmake
@@ -6,7 +6,7 @@ set(CMAKE_SYSTEM_PROCESSOR aarch64)
 # ---- SYSROOT ----
 # Allow override via -DCMAKE_SYSROOT=...
 if(NOT CMAKE_SYSROOT)
-    set(CMAKE_SYSROOT "/home/alex/aarch64/root/")
+    set(CMAKE_SYSROOT "/home/alex/aarch64/root")
 endif()
 
 # ---- COMPILERS ----
@@ -14,13 +14,9 @@ set(CMAKE_C_COMPILER   aarch64-linux-gnu-gcc)
 set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++)
 set(CMAKE_ASM_COMPILER aarch64-linux-gnu-gcc)
 
-# ---- Standard GCC sysroot support ----
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --sysroot=${CMAKE_SYSROOT}")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --sysroot=${CMAKE_SYSROOT}")
-
 # ---- Ensure linker gets sysroot
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--sysroot=${CMAKE_SYSROOT} -L${CMAKE_SYSROOT}/usr/lib")
-set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,--sysroot=${CMAKE_SYSROOT} -L${CMAKE_SYSROOT}/usr/lib")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-L${CMAKE_SYSROOT}/usr/lib")
+set(CMAKE_SHARED_LINKER_FLAGS_INIT "-L${CMAKE_SYSROOT}/usr/lib")
 
 # ---- pkg-config support inside sysroot ----
 set(ENV{PKG_CONFIG_LIBDIR} "${CMAKE_SYSROOT}/usr/lib/pkgconfig:${CMAKE_SYSROOT}/usr/share/pkgconfig")
